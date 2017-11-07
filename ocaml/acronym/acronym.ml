b7cf0568fb2cbcfa182613f86f44b46f628db3d8
Patrick Rogers <patrick@helloresolven.com>
2017-11-06 22:35:30 -0800

    use .[] accessor.

--- ocaml/acronym/acronym.ml
+++ ocaml/acronym/acronym.ml
@@ -4,6 +4,6 @@ let delimiters = [' '; '-']
 
 let acronym title =
     String.split_on_chars title ~on:delimiters
-    |> List.map ~f:(fun s -> String.get s 0)
+    |> List.map ~f:(fun s -> s.[0])
     |> String.of_char_list
     |> String.uppercase

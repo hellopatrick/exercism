e8d329b241b0eacb5772792984ca9a90e83971cb
Patrick Rogers <patrick@gfycat.com>
2017-11-07 10:20:08 -0800

    finish work on all your base.

--- ocaml/all-your-base/all_your_base.ml
+++ ocaml/all-your-base/all_your_base.ml
@@ -2,8 +2,6 @@ open Core_kernel.Std
 
 type base = int
 
-let div_and_rem a b = a / b, a mod b
-
 let from_base digits b =
     List.fold digits ~f:(fun acc d -> acc * b + d) ~init:0
 
@@ -11,19 +9,16 @@ let to_base value new_base =
     let rec aux n b digits =
         match n with
         | 0 -> digits
-        | _ ->
-            let div, rem = div_and_rem n b in
-            aux div b (rem::digits)
+        | _ -> aux (n / b) b ((n mod b)::digits)
     in
     aux value new_base []
-    |> List.rev
 
 
 let convert_bases ~from ~digits ~target =
     if from < 2 || target < 2 then None
     else if List.exists digits ~f:(fun digit -> digit >= from || digit < 0) then None
-    else if from = target then Some digits
+    else if digits = [] then None
     else
-        from_base digits from
-        |> to_base target
-        |> Option.some
+        match from_base digits from with
+        | 0 -> Some [0]
+        | k -> Some (to_base k target)

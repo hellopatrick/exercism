f668fca1ce1398ecf45fbd272691e9038453ee33
Patrick Rogers <patrick@helloresolven.com>
2017-11-06 21:55:29 -0800

    move magic number.

--- ocaml/space-age/space_age.ml
+++ ocaml/space-age/space_age.ml
@@ -3,16 +3,18 @@ type planet = Mercury | Venus
             | Jupiter | Saturn
             | Neptune | Uranus
 
+let earth_year = 31557600.0
+
 let age_on planet age_in_seconds =
-    let multiplier = match planet with
-    | Mercury -> 0.2408467 *. 31557600.0
-    | Venus -> 0.61519726 *. 31557600.0
-    | Earth -> 31557600.0
-    | Mars -> 1.8808158 *. 31557600.0
-    | Jupiter -> 11.862615 *. 31557600.0
-    | Saturn -> 29.447498 *. 31557600.0
-    | Uranus -> 84.016846 *. 31557600.0
-    | Neptune -> 164.79132 *. 31557600.0
+    let year_length = match planet with
+    | Mercury -> 0.2408467 *. earth_year
+    | Venus -> 0.61519726 *. earth_year
+    | Earth -> 1.0 *. earth_year
+    | Mars -> 1.8808158 *. earth_year
+    | Jupiter -> 11.862615 *. earth_year
+    | Saturn -> 29.447498 *. earth_year
+    | Uranus -> 84.016846 *. earth_year
+    | Neptune -> 164.79132 *. earth_year
     in
-    float_of_int age_in_seconds /. multiplier
+    float_of_int age_in_seconds /. year_length
 
